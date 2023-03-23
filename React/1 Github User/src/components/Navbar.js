709e26977f593fe024f46e457ef347bb3763a415
ankit patidar <patiankit6555@gmail.com>
2023-03-23 11:38:54 +0530

    auth

--- React/1 Github User/src/components/Navbar.js
+++ React/1 Github User/src/components/Navbar.js
@@ -3,7 +3,21 @@ import styled from 'styled-components'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const Navbar = () => {
-  return <Wrapper>navbar component</Wrapper>
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+    useAuth0()
+  console.log({ isAuthenticated, user, isLoading })
+  return (
+    <Wrapper>
+      <button onClick={() => loginWithRedirect()}>login</button>
+      <button
+        onClick={() => {
+          logout({ returnTo: window.location.origin })
+        }}
+      >
+        logout
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.nav`
--- React/1 Github User/src/index.js
+++ React/1 Github User/src/index.js
@@ -9,7 +9,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <GithubProvider>
-      <App />
+      <Auth0Provider
+        domain={process.env.REACT_APP_DOMAIN}
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        {console.log('ml', process.env.REACT_APP_DOMAIN)}
+        <App />
+      </Auth0Provider>
     </GithubProvider>
   </React.StrictMode>
 )
