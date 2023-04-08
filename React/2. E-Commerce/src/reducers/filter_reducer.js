3a71276cd5426abc1ff1c618c33f1cd9e06689f7
ankit patidar <patiankit6555@gmail.com>
2023-04-08 22:10:28 +0530

    clear filter

--- React/2. E-Commerce/src/reducers/filter_reducer.js
+++ React/2. E-Commerce/src/reducers/filter_reducer.js
@@ -68,6 +68,20 @@ const filter_reducer = (state, action) => {
   if (action.type === FILTER_PRODUCTS) {
     console.log('ankit')
   }
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      filters: {
+        ...state.filters,
+        text: '',
+        company: 'all',
+        category: 'all',
+        color: 'all',
+        price: state.filters.max_price,
+        shipping: false,
+      },
+    }
+  }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
