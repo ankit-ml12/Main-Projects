f0654ba49e4078876a3306e88ae6bd1392a3864d
ankit patidar <patiankit6555@gmail.com>
2023-04-10 11:27:58 +0530

    auth O

--- React/2. E-Commerce/src/components/CartButtons.js
+++ React/2. E-Commerce/src/components/CartButtons.js
@@ -9,6 +9,7 @@ import { useUserContext } from '../context/user_context'
 const CartButtons = () => {
   const { total_item } = useCartContext()
   const { closeSidebar } = useProductsContext()
+  const { loginWithRedirect, myUser, logout } = useUserContext()
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
@@ -18,9 +19,19 @@ const CartButtons = () => {
           <span className="cart-value">{total_item}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
-        Login <FaUserPlus />
-      </button>
+      {myUser ? (
+        <button
+          type="button"
+          className="auth-btn"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type="button" className="auth-btn" onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   )
 }
